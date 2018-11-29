f6fb4194d9ce5ab70becb0e235bbc7a09d75072c
Raghu Betina <raghu@firstdraft.com>
2018-11-29 00:02:13 +0000

    Whitelist columns in Availability for ActiveAdmin

--- app/admin/availabilities.rb
+++ app/admin/availabilities.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Availability do
+
+ permit_params :user_id, :date, :start_time, :end_time
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
